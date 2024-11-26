9a418aeb2429dcc75d1392f08caf94f515065207
Neduecker <davidneudecker@gmail.com>
2024-11-26 10:26:57 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/jsPoc/",
+  base: "/jsPoC/",
   worker: {
     format: "es"
   },
